c6174ab04c8cf4936e6924bdc8d9c233e3e83901
cataclysm78 <42520898+moshkof@users.noreply.github.com>
2021-04-27 02:32:24 +0300

    Добавление popup с координатами на карту при клике

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ import folium
 maps = folium.Map(location=[59.807602, 30.379634], zoom_start=17, tiles='https://server.arcgisonline.com/ArcGIS/rest'
 																		'/services/World_Imagery/MapServer/tile/{z}/{'
 																		'y}/{x}', attr="toner-bcg")
-maps.add_child(folium.Marker(location=[59.807602, 30.379634], popup="Привет!Здесь живу я!", icon=folium.Icon(color="green")))
+fg = folium.FeatureGroup(name="My map")
+fg.add_child(folium.Marker(location=[59.807602, 30.379634], popup="Привет!Здесь живу я!", icon=folium.Icon(color="green")))
+maps.add_child(fg)
+maps.add_child(folium.LatLngPopup())
+
 maps.save('map.html')
