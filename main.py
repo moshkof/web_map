ef6d6c9c0fe46630463f61535eaec8a38fcbdfce
cataclysm78 <42520898+moshkof@users.noreply.github.com>
2021-04-27 02:11:41 +0300

    Добавление маркера на карту

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import folium
 
-maps = folium.Map(location=[59.803338, 30.377412], zoom_start=17, tiles='https://server.arcgisonline.com/ArcGIS/rest'
+maps = folium.Map(location=[59.807602, 30.379634], zoom_start=17, tiles='https://server.arcgisonline.com/ArcGIS/rest'
 																		'/services/World_Imagery/MapServer/tile/{z}/{'
 																		'y}/{x}', attr="toner-bcg")
+maps.add_child(folium.Marker(location=[59.807602, 30.379634], popup="Привет!Здесь живу я!", icon=folium.Icon(color="green")))
 maps.save('map.html')
