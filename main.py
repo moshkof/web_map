9a573638903dda446094d33ad3853effdaff449d
cataclysm78 <42520898+moshkof@users.noreply.github.com>
2021-04-27 03:08:52 +0300

    Добавление нескольких маркеров через цикл

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import folium
 
 maps = folium.Map(location=[59.807602, 30.379634], zoom_start=17, tiles='https://server.arcgisonline.com/ArcGIS/rest'
-																		'/services/World_Imagery/MapServer/tile/{z}/{'
-																		'y}/{x}', attr="toner-bcg")
+                                                                        '/services/World_Imagery/MapServer/tile/{z}/{'
+                                                                        'y}/{x}', attr="toner-bcg")
 fg = folium.FeatureGroup(name="My map")
-fg.add_child(folium.Marker(location=[59.807602, 30.379634], popup="Привет!Здесь живу я!", icon=folium.Icon(color="green")))
+for coordinates in [[59.807602, 30.379634], [59.807602, 30.38]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="Привет!Здесь живу я!",
+                               icon=folium.Icon(color='red', icon="home", prefix='fa')))
+assert isinstance(fg, object)
 maps.add_child(fg)
 maps.add_child(folium.LatLngPopup())
 
